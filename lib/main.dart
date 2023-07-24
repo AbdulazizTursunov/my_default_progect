7c1949c54ede87bdccfa2a3a76b25fb1b8721e37
Hp Victus <tursunovAbd563@gmail.com>
2023-07-24 17:26:03 +0500

    pagenation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:my_default_progect/ui/pagenation/page_screen.dart';
-import 'animation_screen/animation_home_screen.dart';
-import 'animation_screen/homework_animated_size.dart';
-import 'animation_screen/homework_eight_aling.dart';
-import 'animation_screen/homework_five.dart';
-import 'animation_screen/homework_four.dart';
-import 'animation_screen/homework_one.dart';
-import 'animation_screen/homework_seven_opacity.dart';
-import 'animation_screen/homework_six.dart';
-import 'animation_screen/homework_three.dart';
-import 'animation_screen/homework_two_gif.dart';
-import 'animation_screen/loading_homework.dart';
-import 'animation_screen/text_size_animation_homework.dart';
 
-void main () async{
-  runApp(MyApp());
+
+
+// import 'data/hive_service/hive_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+
 }
+
 
 
 
@@ -29,29 +24,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        appBar: AppBar(title:const Text("pages"),),
-        body: ListView.builder(
-          physics:const   PageScrollPhysics(),
-          itemCount: pages.length,
-          itemBuilder: (context, int index){
-            var page = pages[index];
-            return ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => page,));
-              },
-              title:  Text("$page             ${index + 1}"),
-            );
-          }
-        ),
-
-      ),
-
     );
   }
 }
-
-
-
