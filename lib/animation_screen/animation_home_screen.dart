6718dafa0c6955dfc558aa0774cce5fb0e793674
Hp Victus <tursunovAbd563@gmail.com>
2023-07-24 16:53:12 +0500

    pagenation

--- lib/animation_screen/animation_home_screen.dart
+++ lib/animation_screen/animation_home_screen.dart
@@ -29,3 +29,57 @@ List<Widget> pages = [
   const TextSizeAnimationHomework(),
   const BorderColor(),
 ];
+// import 'package:flutter/material.dart';
+// import 'package:my_default_progect/ui/pagenation/page_screen.dart';
+// import 'animation_screen/animation_home_screen.dart';
+// import 'animation_screen/homework_animated_size.dart';
+// import 'animation_screen/homework_eight_aling.dart';
+// import 'animation_screen/homework_five.dart';
+// import 'animation_screen/homework_four.dart';
+// import 'animation_screen/homework_one.dart';
+// import 'animation_screen/homework_seven_opacity.dart';
+// import 'animation_screen/homework_six.dart';
+// import 'animation_screen/homework_three.dart';
+// import 'animation_screen/homework_two_gif.dart';
+// import 'animation_screen/loading_homework.dart';
+// import 'animation_screen/text_size_animation_homework.dart';
+//
+// void main () async{
+//   runApp(MyApp());
+// }
+//
+//
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return  MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home:Scaffold(
+//         appBar: AppBar(title:const Text("pages"),),
+//         body: ListView.builder(
+//             physics:const   PageScrollPhysics(),
+//             itemCount: pages.length,
+//             itemBuilder: (context, int index){
+//               var page = pages[index];
+//               return ListTile(
+//                 onTap: (){
+//                   Navigator.push(context, MaterialPageRoute(builder: (context) => page,));
+//                 },
+//                 title:  Text("$page             ${index + 1}"),
+//               );
+//             }
+//         ),
+//
+//       ),
+//
+//     );
+//   }
+// }
